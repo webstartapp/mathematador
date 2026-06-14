3eb790f32b28e1a5527a896c493553c4d173efa9
karelbohac <bohac@qualityregistry.org>
2026-06-15 01:22:12 +0200

    fix(server): support static ts path alias resolution for root configuration files

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,10 +1,9 @@
-/* eslint-disable no-restricted-imports */
 import "module-alias/register";
 import "ts-node/register";
 import "tsconfig-paths/register";
 import * as dotenv from "dotenv";
 
-import { configKnex } from "./src/knexWrapper";
+import { configKnex } from "@/knexWrapper";
 
 dotenv.config({ path: ".env" });
 
